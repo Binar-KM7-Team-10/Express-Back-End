3e83bab90890f7ad920fb1e4813c8b0d42271676
Bibbeep <muhammadhabibalfarabi290803@gmail.com>
2024-12-04 03:33:37 +0700

    feature: Added authentication middleware and apply it to schedules routes

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const ScheduleController = require('../controllers/schedule');
+const Auth = require('../middlewares/restrict');
 
 router.get('/schedules', ScheduleController.getAll);
 router.get('/schedules/:id', ScheduleController.getById);
-router.post('/schedules', ScheduleController.create);
-router.delete('/schedules/:id', ScheduleController.delete);
-router.patch('/schedules/:id', ScheduleController.editById);
+router.post('/schedules', Auth.admin, ScheduleController.create);
+router.delete('/schedules/:id', Auth.admin, ScheduleController.delete);
+router.patch('/schedules/:id', Auth.admin, ScheduleController.editById);
 
 module.exports = router;
