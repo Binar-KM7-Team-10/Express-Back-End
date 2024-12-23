6052ed1ae35cbf97af7997d94fb101d96b844af2
Bibbeep <muhammadhabibalfarabi290803@gmail.com>
2024-12-23 13:32:37 +0700

    fix: Remove coverage folder from tracking

--- tests/integration/user.integration.test.js
+++ tests/integration/user.integration.test.js
@@ -22,8 +22,8 @@ async function resetDatabase() {
 }
 
 const generateToken = (payload, expiresIn = '7d') => {
-        return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
-    };
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
+};
 
 describe('GET User Testing', () => {
     let adminUser, buyerUser;
